27bef536e300548b679e6a6357f7c662a24fb4c0
Andrey_ka66 <chelentano1711@gmail.com>
2021-10-07 19:05:06 +0300

    Imported Movies List

--- src/components/ImportationMovies.js
+++ src/components/ImportationMovies.js
@@ -17,6 +17,32 @@ const ImportationMovies = () => {
     <div>
       <input className="mb-2" type="file" id="importId" onChange={sentMovies} />
       {importedMovies.length > 0 ? <h2>Successfully Imported</h2> : ""}
+      {importedMovies.length > 0 ? (
+        <table className="table text-light table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Title</th>
+              <th scope="col">Year</th>
+              <th scope="col">Format</th>
+            </tr>
+          </thead>
+          {importedMovies
+            ? importedMovies.map((importedMovie) => (
+                <tbody key={importedMovie.id}>
+                  <tr>
+                    <td>{importedMovie.id}</td>
+                    <td>{importedMovie.title}</td>
+                    <td>{importedMovie.year}</td>
+                    <td>{importedMovie.format}</td>
+                  </tr>
+                </tbody>
+              ))
+            : ""}
+        </table>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
